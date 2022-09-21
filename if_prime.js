aa884e7e42dd66bfd3ce7c759cd459ec003afdab
amolthakare <100188653+amolthakare@users.noreply.github.com>
2022-09-21 23:12:59 +0530

    code fixed by amol

--- if_prime.js
+++ if_prime.js
@@ -1,5 +1,17 @@
-function if_prime(x){
-    for (k=2; k<x; k++){
-        
+var n;
+function IdentifyPrime(n){
+    var count=0;
+    for(var i=2;i<n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+    if(count>0){
+        console.log(n+" "+"is prime");
+    }
+    else{
+        console.log(n+" "+"is not prime");
     }
 }
+
+IdentifyPrime(6);
